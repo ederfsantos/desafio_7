ef4e1ccbb9a99df975a5a1d3a959b4ff7e319b1a
Eder Ferreira Santos <ederfsantos@live.com>
2025-03-06 11:52:12 -0300

    removendo codigos comentados

--- desafio7.js
+++ desafio7.js
@@ -2,13 +2,11 @@
 let numero1;
 let numero2;
 let opcao;
-let opc;
 
 do {
 
     opcao = parseInt(prompt("Calculadora 7 Days Of Code - JS JavaScript\nEscolha uma operação matemática:\n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair"));
-    // numero1 = parseInt(prompt("Informe o primeiro número para a operação escolhida:"));
-    //numero2 = parseInt(prompt("Informe o segundo número para a operação escolhida:"));
+
     switch (opcao) {
 
         case 1:
